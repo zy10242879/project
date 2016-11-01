f32642915236f8108c2640cd8a1ad45170cb7f70
zy10242879 <zy10242879@gmail.com>
2016-11-01 09:30:54 +0800

    index

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">start go to Laravel 5 OK OK!</div>
+                <div class="title">Laravel 5 OK OK!</div>
             </div>
         </div>
     </body>
