bace20c1dc0fe478e6775edf6ebda66cb1f865c5
zy10242879 <zy10242879@gmail.com>
2016-11-03 09:29:19 +0800

    create ProductControlle->actionDetail & product/detail.php

--- Yii2/frontend/controllers/ProductController.php
+++ Yii2/frontend/controllers/ProductController.php
@@ -3,7 +3,12 @@ namespace frontend\controllers;
 
 use yii\web\Controller;
 class ProductController extends Controller{
+  //商品分类页
   public function actionCategory(){
     return $this->renderPartial('category');
+  }
+  //商品详情页
+  public function actionDetail(){
+    return $this->render('detail');
   }
 }
