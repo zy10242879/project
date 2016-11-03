e63ab9ce52e96577b79c4ce245503fca2a68a7a4
zy10242879 <zy10242879@gmail.com>
2016-11-03 10:31:58 +0800

    create order actionOrder view/order/index(加载订单中心页面)

--- Yii2/frontend/controllers/OrderController.php
+++ Yii2/frontend/controllers/OrderController.php
@@ -2,8 +2,13 @@
 namespace frontend\controllers;
 use yii\web\Controller;
 class OrderController extends Controller{
+  public $layout = false;  //关闭页头脚
+  //订单中心
+  public function actionIndex(){
+    return $this->render('index');
+  }
   //收银台核对
   public function actionCheck(){
-    return $this->renderPartial('check');
+    return $this->render('check');
   }
 }
